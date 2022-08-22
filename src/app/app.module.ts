dcf543e9f89d3238cbefd2cc700fc697cffec137
Lukas Kolbe <kolbe.lukas@gmx.de>
2022-08-22 14:03:20 +0200

    added landingpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LandingPageComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
